543ad42a2be0681865d86e308bdbdbc02f224273
Mila <morgenglow@gmail.com>
2020-01-22 22:50:53 +0300

    ДЗ к уроку 3 Java, доп. задачи очень интересные, но на них вообще нет времени=(

--- src/java/lesson3/homework/HomeWork.java
+++ src/java/lesson3/homework/HomeWork.java
@@ -2,19 +2,37 @@ package lesson3.homework;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class HomeWork {
+    public static Scanner sc = new Scanner(System.in);
+    
+    //угадай число
+    public static void guessThenumber() {
+        do {
+            System.out.println("\nУгадай число от 0 до 9. У тебя 3 попытки!\n");
+            int number = new Random().nextInt(10);
+            int i;
+            for (i = 1; i < 4; i++) {
+                System.out.printf("\n Введи число, попытка: " + i + ":");
+                int idea = sc.nextInt();
+                if (number > idea) {
+                    System.out.println("Загаданное число больше!\n");
+                } else if (idea < number) {
+                    System.out.println("Загаданное число меньше!\n");
+                } else {
+                    System.out.println("\nУгадал!\n");
+                    break;
+                }
+            }
+            if (i == 4) {
+                System.out.println("Попытки закончились!\n");
+            }
 
-    /* example:
-     * input: Mama mila ramu, Ramu mila mama!!!, [mama, ramu]
-     * output: 4
-     * use regex, split, lowerCase, replace methods
-     * */
-    public int countOfWordsFromDictionaryInString(String input, String[] dictionary) {
-        //посчитать все слова из словаря в input
-        //если одно слово встречается 5 раз его нужно посчитать 5 раз
-        // TODO: 1/21/2020
-        return 0;
+            System.out.print("Поиграем еще? 1 - Да/ 0 - Нет: ");
+
+        } while (sc.nextInt() == 1);
+
     }
 
     /* example:
@@ -22,50 +40,76 @@ public class HomeWork {
      * output: 3
      * use sort
      * */
-    public int kOrderValue(int[] array, int k) {
-        //метод должен вернуть К по порядку элемент массива
-        return 0;
+    public static int korderValue(int[] array, int k) {
+       Arrays.sort(array);
+       return array[k-1];
     }
 
-    /*
-    *  Это реальная задача, которую я сегодня делал на работе
-    *  Кому интересно, можете проверить свои силы))))
-    *  Если слово из values есть в словаре from, его необходимо заменить
-    *  с from[i] на to[i] [a, b, c], [a, b], [x, y] -> [x, y, c]
-    *  Если словарь to длиннее from, то строка to[from.length] - дефолтное
-    *  значение для всех values, которых нет в словаре from
-    *  [a, b, c, d], [a, b], [x, y, lol] -> [x, y, lol, lol]
-    *  Если словарь from длиннее to, то необходимо удалить из values все значения
-    *  имющиеся в куске from на индексах от to.length до rom.length
-    *  [a, b, c, d], [a, b, e, d], [x, y] -> [x, y, c] d удалем, так как он есть во
-    *  from
-     */
-    public String[] translate(String[] values, String[] from, String[] to) {
-        // TODO: 1/22/2020
-        return null;
+    //реализовала абсолютно неинтересную игру, где попытки записываются в массив, потому что не поняла условия - ночь и т.д.
+    //но в качестве опыта интересно, а аналог с тремя попытками в методе guessThenumber()
+    public static void binarySearchGame() {
+        do {
+            int number = new Random().nextInt(100);
+            System.out.println("\nУгадай число от 0 до 100. У тебя 7 попыток!\n");
+            int size = sc.nextInt();
+            if (size>7) {
+                size=7;
+            }
+            int [] answers = new int[size];
+            for (int i = 0; i < size; i++) {
+                answers[i] = sc.nextInt();
+                if (number > answers[i]) {
+                    System.out.println(answers[i]+": Загаданное число больше!\n");
+                } else if (answers[i] > number) {
+                    System.out.println(answers[i]+": Загаданное число меньше!\n");
+                } else {
+                    System.out.println(answers[i]+ ": Угадал!\n");
+                    break;
+                }
+            }
+            System.out.print("Поиграем еще? 1 - Да/ 0 - Нет: ");
+
+        } while (sc.nextInt() == 1);
     }
 
-    /* example:
-     * input: m.levin.main@mailg.spb.com
-     * output: true
-     *
-     * use matches
-     * */
-    public boolean isEmail(String input) {
-        // TODO: 1/21/2020
-        return false;
+    //угадай слово
+    public static void guessWord() {
+
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+                "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        do {
+            System.out.println("\n Угадай, какое слово из списка загадано!");
+            System.out.println(Arrays.toString(words));
+            String answer = words[new Random().nextInt(words.length)];
+            char[] attempt = new char[answer.length()];
+            Arrays.fill(attempt, '*');
+
+            while (true) {
+                System.out.print("\n Введи слово: ");
+                String proposal = sc.next().toLowerCase();
+                if (answer.equals(proposal)) {
+                    System.out.println("\nУгадал!\n");
+                    break;
+                }
+                for (int i = 0; i < answer.length(); i++)
+                    if (answer.charAt(i) == proposal.charAt(i)) attempt[i] = answer.charAt(i);
+                System.out.println(attempt);
+            }
+
+            System.out.print("Поиграем еще? 1 - Да/ 0 - Нет: ");
+
+        } while (sc.nextInt() == 1);
+
     }
 
-    public void binarySearchGame() {
-        int secretValue = new Random().nextInt(100); // компьютер загадывает число
-        // TODO: 20.01.2020
-        //мы печатаем в консоль наши предположения (в цикле), компютер отвечает > , < или =
-        //на каждый наш вопрос
-        //если задано более 7 вопросов и не угадано значение, мы проиграли
-        //если компютер вывел =, мы выиграли
-    }
-
-    public void wordsGame() {
-        // TODO: 20.01.2020  
-    }
+public static void main(String[] args) {
+    int [] array = new int[]{1,0,3,17,2,7,14,1,1,7};
+    int k =6;
+    System.out.println(korderValue(array,k));
+    guessThenumber();
+    binarySearchGame();
+    guessWord();
+}
 }
